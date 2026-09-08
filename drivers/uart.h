0df8f536315dd80ff5ff5d660fce60ac4ad3bc04
David Voronov <davidvoronof@gmail.com>
2026-09-07 19:10:32 -0700

    headers for uart drivers

--- drivers/uart.h
+++ drivers/uart.h
@@ -2,10 +2,10 @@
 #define UART_H
 
 // One time configuration of UART registers
-void enable_uart();
+void uart_init();
 
-void uart_printf(char* format, ...);
+void uart_printf(char *format, ...);
 
-
+void uart_print_byte(char b);
 
 #endif // !UART_H
--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include "stm32f446xx.h"
 #include <stdbool.h>
 
+volatile int milliseconds;
+
+void SysTick_Handler() { milliseconds++; }
+
+void delay(int ms) {
+  int start = milliseconds;
+  while (milliseconds < (start + ms)) {
+  }
+}
+
+static void system_init(void) {
+  SystemCoreClockUpdate();
+
+  SysTick_Config(SystemCoreClock / 1000u);
+}
+
 int main(void) {
+  system_init(); // Configures the clock to tick every 1ms
+
   // Enable the clock on the GPIOA bus
   RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN_Msk;
 
@@ -10,17 +28,23 @@ int main(void) {
   GPIOA->MODER &= ~(3u << 10); // set bits 10-11 to 00
   GPIOA->MODER |= (1u << 10);  // set bits 10-11 to 01 (output)
   GPIOA->OTYPER &= ~(1u << 5); // set bit 5 to 0
+  // GPIOA->OSPEEDR &= ~(3u << 10); // set speed bits to 00
+  // GPIOA->OSPEEDR |= (1u << 00); // low speed (00)
+  // GPIOA->OSPEEDR |= (1u << 10);  // medium speed (01)
+  // GPIOA->OSPEEDR |= (1u << 11); // high speed (11)
 
   while (true) {
     // GPIOA->ODR ^= (1u << 5);
 
     GPIOA->BSRR |= (1u << 5); // set pin 5
-    for (volatile int i = 0; i < 50000; i++) {
-    }
+    delay(1000);
+    // for (volatile int i = 0; i < 50000; i++) {
+    // }
 
     GPIOA->BSRR |= (1u << (16 + 5));
+    delay(1000);
 
-    for (volatile int i = 0; i < 500000; i++) {
-    }
+    // for (volatile int i = 0; i < 500000; i++) {
+    // }
   }
 }
