7109d43a191e9c7b4806166a1a4b91dd8686c7d9
Denodov <denysodov@gmail.com>
2026-09-16 13:12:56 -0700

    fix gyro

--- stm32f446/src/main.c
+++ stm32f446/src/main.c
@@ -36,11 +36,19 @@ static void system_init(void) {
 
 int main(void) {
   system_init(); // Configures the clock to tick every 1ms
+
+  uart_printstr("Keep MPU still! Calibrating...");
+  GPIOA->BSRR |= (1u << 5); // turn LED on while calibrating
+
+  imu_calibrate_gyro();
+
+  GPIOA->BSRR |= (1u << (16 + 5)); // turn LED off
+  uart_printstr("Calibration complete.\r\n");
+
   float x_accel, y_accel, z_accel, roll, pitch, yaw; 
 
   while (true) {
     // GPIOA->ODR ^= (1u << 5);
-
     x_accel = get_x_accel();
     y_accel = get_y_accel();
     z_accel = get_z_accel();
@@ -48,21 +56,15 @@ int main(void) {
     pitch = get_pitch();
     yaw = get_yaw();
 
-    uart_printf("\x1b[2Kx: %.2f g\r\n", x_accel);
-    uart_printf("\x1b[2Ky: %.2f g\r\n", y_accel);
-    uart_printf("\x1b[2Kz: %.2f g\r\n", z_accel);
-    uart_printf("\x1b[2Kroll: %.2f deg/s\r\n", roll);
-    uart_printf("\x1b[2Kpitch: %.2f deg/s\r\n", pitch);
-    uart_printf("\x1b[2Kyaw: %.2f deg/s\r", yaw);
+    uart_printf("\x1b[2K\rx:     %.2f g\r\n", x_accel);
+    uart_printf("\x1b[2K\ry:     %.2f g\r\n", y_accel);
+    uart_printf("\x1b[2K\rz:     %.2f g\r\n", z_accel);
+    uart_printf("\x1b[2K\rroll:  %.2f deg/s\r\n", roll);
+    uart_printf("\x1b[2K\rpitch: %.2f deg/s\r\n", pitch);
+    uart_printf("\x1b[2K\ryaw:   %.2f deg/s\r\n", yaw);
 
-    uart_printf("\x1b[5A"); // Move up 5 lines
-
-    // delay(50);
-    // Blink LED code
-    GPIOA->BSRR |= (1u << 5); // set pin 5
-    delay(100);
-    GPIOA->BSRR |= (1u << (16 + 5));
-    delay(100);
+    uart_printstr("\x1b[6A");
+    delay(250);
 
   }
 }
